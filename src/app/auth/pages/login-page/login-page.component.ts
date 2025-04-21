efae1eb50ffb87f2ac40c74c75d557cbcb317129
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-21 11:09:45 -0400

    servicio auth con login, resp y conexion al backend para enviar la peticion

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -11,18 +12,21 @@ export class LoginPageComponent {
   hasError = signal(false);
   isPosting = signal(false);
 
+  authService = inject(AuthService);
+
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  
   onSubmit() {
     if (this.loginForm.invalid) {
       this.hasError.set(true);
       return;
     }
-    const {email = '', password = ''} = this.loginForm.value
-    console.log({email, password});
+    const { email = '', password = '' } = this.loginForm.value;
+    this.authService.login(email!, password!).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { AuthResponse } from '@auth/interfaces/auth-response.interface';
+import { User } from '@auth/interfaces/user.interface';
+import { tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+const baseUrl = environment.baseUrl;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _authStatus = signal<AuthStatus>('checking');
+  private _user = signal<User | null>(null);
+  private _token = signal<string | null>(null);
+
+  private http = inject(HttpClient);
+
+  authStatus = computed<AuthStatus>(() => {
+    if (this._authStatus() === 'checking') return 'checking';
+    if (this._user()) {
+      return 'authenticated';
+    }
+
+    return 'not-authenticated';
+  });
+
+  user = computed<User | null>(() => this._user());
+  token = computed<string | null>(() => this._token());
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponse>(`${baseUrl}/auth/login`, {
+      email,
+      password,
+    }).pipe(
+      tap((resp) => {
+        this._user.set(resp.user);
+        this._token.set(resp.token);
+        this._authStatus.set('authenticated');
+
+        localStorage.setItem('token', resp.token);
+      })
+    );
+  }
+}
