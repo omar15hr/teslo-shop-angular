4ed0af71ee8fa08c3947fd01dd6d2431a34f9880
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-21 17:50:45 -0400

    autorizacion de admin guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -34,6 +34,7 @@ export class AuthService {
 
   user = computed<User | null>(() => this._user());
   token = computed<string | null>(() => this._token());
+  isAdmin = computed(() => this.user()?.roles.includes('admin') ?? false);
 
   login(email: string, password: string): Observable<boolean> {
     return this.http
