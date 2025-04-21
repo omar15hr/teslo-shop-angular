e920c6be2c9e2177600417ae43f07e133079cea9
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-21 09:15:54 -0400

    separacion de logica servicio de paginacion

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
 import { LoaderComponent } from '@shared/components/loader/loader.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -14,20 +13,10 @@ import { LoaderComponent } from '@shared/components/loader/loader.component';
 })
 export class HomePageComponent {
   productsService = inject(ProductsService);
-  activatedRoute = inject(ActivatedRoute);
-
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map((params) => (params.get('page') ? +params.get('page')! : 1)),
-      map((page) => (isNaN(page) ? 1 : page))
-    ),
-    {
-      initialValue: 1,
-    }
-  );
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    request: () => ({ page: this.currentPage() - 1 }),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
       return this.productsService.getProducts({
         offset: request.page * 9,
